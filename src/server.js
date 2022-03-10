4910f88e0c5897875fd7903601755e2c9cae8855
siraj-yusuf <yusuf@skystoptechnologies.com>
2022-03-10 21:38:36 +0530

    second commit

--- src/server.js
+++ src/server.js
@@ -1,6 +1,11 @@
 const app = require('./app')
 const port = process.env.PORT || 3000
 
+app.get('/',(req,res)=>{
+    res.send('App is up and running')
+})
+
+
 // port listening
 app.listen(port,() => {
     console.log('Server is up on port ' + port)
